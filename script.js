3046099bd67178f3e213ac52b4dbf7ab599f8c8b
blackmida <blackmida@gmail.com>
2020-06-13 00:26:45 +0200

    Added modalAddTerm

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 // Get the modal
 var modalStartGame = document.getElementById("modalStartGame");
 var modalUsername = document.querySelector('#modalUsername');
+var modalAddTerm = document.querySelector('#modalAddTerm');
 
 // Get the button that opens the modal
 var buttonStartGame = document.getElementById("buttonStartGame");
 var buttonLogin = document.querySelector('#buttonLogin');
+var buttonAddTerm = document.querySelector('#buttonAddTerm');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var closeUsername = document.querySelector('#closeUsername');
+var closeAddTerm = document.querySelector('#closeAddTerm');
 
 // When the user clicks the button, open the modal 
 buttonStartGame.onclick = function() {
@@ -21,6 +24,10 @@ buttonLogin.onclick = function(){
   buttonLogin.style.outline = 'none';
 }
 
+buttonAddTerm.addEventListener('click', () => {
+  modalAddTerm.style.display = 'block';
+})
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modalStartGame.style.display = "none";
@@ -28,12 +35,16 @@ span.onclick = function() {
 closeUsername.onclick = function(){
   modalUsername.style.display = "none";
 }
+closeAddTerm.onclick = function(){
+  modalAddTerm.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modalStartGame || event.target == modalUsername) {
+  if (event.target == modalStartGame || event.target == modalUsername || event.target == modalAddTerm) {
     modalStartGame.style.display = "none";
     modalUsername.style.display = "none";
+    modalAddTerm.style.display = "none";
   }
 }
 
