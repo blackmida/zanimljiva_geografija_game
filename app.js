e33d2f392e04f1d4cd19c3846cec8556322522e1
blackmida <blackmida@gmail.com>
2020-07-09 15:56:28 +0200

    Function for adding term in database

--- app.js
+++ app.js
@@ -2,45 +2,60 @@ import { Term } from "./term.js";
 
 
 // DOM ELEMENTS
-const buttonUpdateUsername = document.querySelector('#buttonUpdateUsername');
+// const buttonUpdateUsername = document.querySelector('#buttonUpdateUsername');
+const formUpdateUsername = document.querySelector('#formUpdateUsername');
 let inputUsername = document.querySelector('#inputUsername');
 let usernameError = document.querySelector('#usernameError');
 
-let formAddTerm = document.querySelector('#formAddTerm');
+
+const formAddTerm = document.querySelector('#formAddTerm');
 let inputTerm = document.querySelector('#inputTerm');
+let selectCategoryAddTerm = document.querySelector('#selectCategoryAddTerm');
 
 let usernameModal = document.querySelector('#modalUsername');
 // RegEx for Username
 const usernameRegexp = /^\S*$/;
 
 
-// Here is the button which create/update username
-buttonUpdateUsername.addEventListener('click', () => {
+formUpdateUsername.addEventListener('submit', (e) => {
+    e.preventDefault();
     let username = inputUsername.value;
-    if(usernameRegexp.test(username)){
+    if (usernameRegexp.test(username)) {
         localStorage.setItem('usernameLS', username);
         usernameError.innerHTML = `Dobrodosao ${username}!`;
-        setTimeout(function(){
+        // OVDE JE POKUSAJ STILIZACIJE PORUKE PRILIKOM PRIJAVE
+        // usernameError.innerHTML = `<p style="color: green;">Dobrodosao ${username}!</p>Dobrodosao ${username}!`;
+        setTimeout(function () {
             usernameError.innerHTML = '';
             usernameModal.style.display = "none";
         }, 2000);
 
     }
-    else{
+    else {
         inputUsername.value = '';
         usernameError.innerHTML = 'Username ne sme da ima space (prazna polja).';
-        setTimeout(function(){
+        setTimeout(function () {
             usernameError.innerHTML = '';
-            
+
         }, 2000);
     }
 });
 
 formAddTerm.addEventListener('submit', (e) => {
     e.preventDefault();
-    // Check and replace everything that is not letter
-    // Check and repalce all underscores
-    let term = inputTerm.value.replace(/[^A-Ž]+/gi, "").replace(/[_]+/gi, "");
-    term = term[0].toUpperCase() + term.slice(1).toLowerCase();
-    console.log(term);
+    if (selectCategoryAddTerm.value != '') {
+        // Check and replace everything that is not letter
+        // Check and repalce all underscores
+        let termFromForm = inputTerm.value.replace(/[^A-Ž]+/gi, "").replace(/[_]+/gi, "");
+        termFromForm = termFromForm[0].toUpperCase() + termFromForm.slice(1).toLowerCase();
+
+        let category = selectCategoryAddTerm.value;
+
+        let term = new Term(category, termFromForm);
+        term.checkTermInDatabase();
+    }
+    else{
+        alert('Morate da izaberete kategoriju!')
+    }
+    formAddTerm.reset();
 })
